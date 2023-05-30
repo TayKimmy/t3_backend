0a8e98c5c1afc2ad20a9c90dab8b3e830c6370af
Soham360 <sohamkamat10@gmail.com>
2023-05-30 15:31:41 -0700

    j

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
-from model.basketballs import initBasketballs
 
 
 # setup APIs
@@ -16,7 +15,6 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
-from api.basketball import basketball_api
 from api.graph import graph_api
 
 
@@ -28,7 +26,6 @@ app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
-app.register_blueprint(basketball_api)
 app.register_blueprint(graph_api)
 app.register_blueprint(app_projects) # register app pages
 
@@ -51,7 +48,6 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
-    initBasketballs()
 
 # this runs the application on the development server
 if __name__ == "__main__":
